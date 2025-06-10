830bc29cf018c9f1531b6f35837cf8005a24863e
michaliskaratsiorispartner <michalis.karatsioris@partner.bmwgroup.com>
2025-06-10 17:20:44 +0200

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,12 +5,10 @@ export const Footer = () => {
   return (
     <footer className="p-4 bg-white shadow md:flex md:items-center md:justify-center dark:bg-gray-800">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-        <Link to="/" className="flex items-center">
-          <img src={LogoImg} className="mr-2 h-8 sm:h-9" alt="CineFriend Logo" />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            CineFriend
-          </span>
-        </Link>
+        <img src={LogoImg} className="mr-2 h-8 sm:h-9" alt="CineFriend Logo" />
+        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+          CineFriend
+        </span>
         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
           <li>
             <a
