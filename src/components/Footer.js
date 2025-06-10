b9aa8a02649239b76dcff73f59e1e1b3c770b0f7
michaliskaratsiorispartner <michalis.karatsioris@partner.bmwgroup.com>
2025-06-10 17:18:09 +0200

    updated logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
+import LogoImg from "../assets/favicon.ico";
 
 export const Footer = () => {
   return (
     <footer className="p-4 bg-white shadow md:flex md:items-center md:justify-center dark:bg-gray-800">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2025 CineFriend. All Rights Reserved.
-        </span>
+        <Link to="/" className="flex items-center">
+          <img src={LogoImg} className="mr-2 h-8 sm:h-9" alt="CineFriend Logo" />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            CineFriend
+          </span>
+        </Link>
         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
           <li>
             <a
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import { Link, NavLink, useNavigate } from "react-router-dom";
-import Logo from "../assets/favicon.ico";
+import LogoImg from "../assets/favicon.ico";
 
 export const Header = () => {
   const [hidden, setHidden] = useState(true);
@@ -37,7 +37,7 @@ export const Header = () => {
       <nav className="bg-headerbg border-b-2 border-gray-200 px-2 sm:px-4 py-2 dark:bg-gray-900 dark:border-b-1 dark:border-gray-900">
         <div className="flex flex-wrap justify-between items-center mx-auto">
           <Link to="/" className="flex items-center">
-            <img src={Logo} className="mr-2 h-8 sm:h-9" alt="Cinemate Logo" />
+            <img src={LogoImg} className="mr-2 h-8 sm:h-9" alt="CineFriend Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               CineFriend
             </span>
